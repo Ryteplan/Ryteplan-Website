87b9b21f8e2c5828e49f8d5ba1ae255e24d5d1fc
Brandon Scott <brandonscott@gmail.com>
2025-11-30 21:01:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { BuilderComponent, builder } from '@builder.io/react';
 import { usePathname } from 'next/navigation';
 
-builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY || '');
 
 export default function CatchAllPage() {
   const pathname = usePathname();
