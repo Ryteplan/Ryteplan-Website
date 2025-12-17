0e1cf8dc0bfa7460aa34ea8264b4c95dabef8bee
Brandon Scott <brandonscott@gmail.com>
2025-12-17 13:20:54 -0500

    Create edit-symbol.tsx

---
+++ pages/edit-symbol.tsx
@@ -0,0 +1,8 @@
+import { BuilderComponent, builder } from '@builder.io/react';
+
+// Replace with your Public API Key.
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+export default function Page() {
+  return <BuilderComponent model="symbol" />
+}
