ed97fefb4f098ed79c89a128fcb8ab9ab3fa57fe
Brandon Scott <brandonscott@gmail.com>
2025-12-17 13:53:12 -0500

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
   params: Promise<{ slug: string }>;
 }
 
-export const runtime = 'experimental-edge';
+export const runtime = 'edge';
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
